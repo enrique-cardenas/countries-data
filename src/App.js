e425bed3120f05eec198161f943fce623ecb72a5
Enrique <enriquecardenas96@gmail.com>
2019-11-07 15:25:49 -0800

    add code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import Filter from './components/Filter'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+
+  const [ countries, setCountries] = useState([]) 
+  const [ newFilter, setNewFilter ] = useState('')
+
+  useEffect( () => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        setCountries(response.data)
+      })
+
+  }, [])
+
+  const countriesToShow = countries.filter(country => country.name.toLowerCase().includes(newFilter.toLowerCase()))
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
+  const displayCountries = () => {
+    if(countriesToShow.length > 10){
+      return(
+        <p>Too many matches, specify another filter</p>
+      )
+    }
+    else if(countriesToShow.length > 1){
+      return(
+        countriesToShow.map(country => <p key={country.name}>{country.name}</p>)
+      )
+    }
+    else if(countriesToShow.length === 1){
+      const country = countriesToShow[0]
+      return(
+        <>
+          <h1>{country.name}</h1>
+          <p>capital is {country.capital}</p>
+          <p>population {country.population}</p>
+          <h2>languages</h2>
+          <ul>
+            {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
+          </ul>
+          <img src={country.flag} alt={`${country.name}'s flag`} height="80" width="100" />
+        </>
+      )
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Filter newFilter={newFilter} handleFilterChange={handleFilterChange}/>
+      <Countries countries={countriesToShow}/>
+      {displayCountries()}
     </div>
   );
 }
